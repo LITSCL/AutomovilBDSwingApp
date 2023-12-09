14f2ea85f27bff98d948742a6dbc8db584d85b1b
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-09 14:40:53 -0300

    Ok

--- AutomovilBDSwingApp/src/cl/inacap/automovilswingapp/StartFrame.java
+++ AutomovilBDSwingApp/src/cl/inacap/automovilswingapp/StartFrame.java
@@ -20,10 +20,10 @@ import java.awt.event.ActionEvent;
 import javax.swing.JDesktopPane;
 
 public class StartFrame extends JFrame {
-	private IngresarAutomovilFrame ingresarAutomovilFrame; //Relaci�n de tipo HAS-A (Porque el JFrame tiene JInternalFrame).
-	private MostrarAutomovilFrame mostrarAutomovilFrame; //Relaci�n de tipo HAS-A (Porque el JFrame tiene JInternalFrame).
-	private EliminarAutomovilFrame eliminarAutomovilFrame; //Relaci�n de tipo HAS-A (Porque el JFrame tiene JInternalFrame).
-	private ModificarAutomovilFrame modificarAutomovilFrame; //Relaci�n de tipo HAS-A (Porque el JFrame tiene JInternalFrame).
+	private IngresarAutomovilFrame ingresarAutomovilFrame; //Relación de tipo HAS-A (Porque el JFrame tiene JInternalFrame).
+	private MostrarAutomovilFrame mostrarAutomovilFrame; //Relación de tipo HAS-A (Porque el JFrame tiene JInternalFrame).
+	private EliminarAutomovilFrame eliminarAutomovilFrame; //Relación de tipo HAS-A (Porque el JFrame tiene JInternalFrame).
+	private ModificarAutomovilFrame modificarAutomovilFrame; //Relación de tipo HAS-A (Porque el JFrame tiene JInternalFrame).
 	private JPanel contentPane;
 	private JDesktopPane desktopPane;
 
@@ -40,7 +40,7 @@ public class StartFrame extends JFrame {
 		});
 	}
 
-	//Proceso de creaci�n del frame.
+	//Proceso de creación del frame.
 	public StartFrame() {
 		setTitle("AutomovilSwingApp"); //Es es el titulo del frame, se muestra al lado del icono cuando se ejecuta el programa. 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -52,24 +52,24 @@ public class StartFrame extends JFrame {
 		JMenu mnOpciones = new JMenu("Opciones");
 		menuBar.add(mnOpciones);
 		
-		//Creaci�n del JMenuItem (Ingresar Automovil).
+		//Creación del JMenuItem (Ingresar Automovil).
 		JMenuItem mntmIngresarAutomovil = new JMenuItem("Ingresar Automovil"); //Aca se crea el JMenuItem llamado (Ingresar Automovil).
-		mntmIngresarAutomovil.addActionListener(e -> showFrameIngresarAutomovil(e)); //Aca se a�ade el Listener del JmenuItem llamado (Ingresar Automovil) el cual llama al m�todo llamado showFrameIngresarAutomovil().
+		mntmIngresarAutomovil.addActionListener(e -> showFrameIngresarAutomovil(e)); //Aca se añade el Listener del JmenuItem llamado (Ingresar Automovil) el cual llama al método llamado showFrameIngresarAutomovil().
 		mnOpciones.add(mntmIngresarAutomovil);
 		
-		//Creaci�n del JMenuItem (Mostrar Automoviles).
+		//Creación del JMenuItem (Mostrar Automoviles).
 		JMenuItem mntmMostrarAutomoviles = new JMenuItem("Mostrar Automoviles"); //Aca se crea el JMenuItem llamado (Mostrar Automovil).
-		mntmMostrarAutomoviles.addActionListener(e -> showFrameMostrarAutomovil(e)); //Aca se a�ade el Listener del JmenuItem llamado (Mostrar Automovil) el cual llama al m�todo llamado showFrameMostrarAutomovil().
+		mntmMostrarAutomoviles.addActionListener(e -> showFrameMostrarAutomovil(e)); //Aca se añade el Listener del JmenuItem llamado (Mostrar Automovil) el cual llama al método llamado showFrameMostrarAutomovil().
 		mnOpciones.add(mntmMostrarAutomoviles);
 		
-		//Creaci�n del JMenuItem (Modificar Automovil).
+		//Creación del JMenuItem (Modificar Automovil).
 		JMenuItem mntmModificarAutomoviles = new JMenuItem("Modificar Automoviles"); //Aca se crea el JMenuItem llamado (Modificar Automoviles).
-		mntmModificarAutomoviles.addActionListener(e -> showFrameModificarAutomoviles(e)); //Aca se a�ade el Listener del JmenuItem llamado (Modificar Automoviles) el cual llama al m�todo llamado showFrameModificarAutomoviles().
+		mntmModificarAutomoviles.addActionListener(e -> showFrameModificarAutomoviles(e)); //Aca se añade el Listener del JmenuItem llamado (Modificar Automoviles) el cual llama al método llamado showFrameModificarAutomoviles().
 		mnOpciones.add(mntmModificarAutomoviles);
 		
-		//Creaci�n del JMenuItem (Eliminar Automovil).
+		//Creación del JMenuItem (Eliminar Automovil).
 		JMenuItem mntmEliminarAutomovil = new JMenuItem("Eliminar Automovil"); //Aca se crea el JMenuItem llamado (Eliminar Automovil).
-		mntmEliminarAutomovil.addActionListener(e -> showFrameEliminarAutomovil(e)); //Aca se a�ade el Listener del JmenuItem llamado (Eliminar Automovil) el cual llama al m�todo llamado showFrameMostrarAutomovil().
+		mntmEliminarAutomovil.addActionListener(e -> showFrameEliminarAutomovil(e)); //Aca se añade el Listener del JmenuItem llamado (Eliminar Automovil) el cual llama al método llamado showFrameMostrarAutomovil().
 		mnOpciones.add(mntmEliminarAutomovil);
 		
 		contentPane = new JPanel();
@@ -83,43 +83,43 @@ public class StartFrame extends JFrame {
 
 	//Codigo a ejecutar del Listener del componente JMenuItem llamado (Ingresar Automovil).
 	private void showFrameIngresarAutomovil(ActionEvent e) {
-		if(this.ingresarAutomovilFrame != null) { //Verifica si el usuario ya habia abierto el frame antes (Esta es la variable que est� arriba como atributo).
-			desktopPane.remove(this.ingresarAutomovilFrame); //Esta instrucci�n elimina el frame existente si el usuario ya lo tenia abierto (Se debe utilizar el objeto desktopPane porque es all� donde st� agregado el frame).
+		if (this.ingresarAutomovilFrame != null) { //Verifica si el usuario ya habia abierto el frame antes (Esta es la variable que está arriba como atributo).
+			desktopPane.remove(this.ingresarAutomovilFrame); //Esta instrucción elimina el frame existente si el usuario ya lo tenia abierto (Se debe utilizar el objeto desktopPane porque es allí donde está agregado el frame).
 		}
-		this.ingresarAutomovilFrame = new IngresarAutomovilFrame(); //Proceso de creaci�n del nuevo frame (Ya que el anterior se borr�).
-		desktopPane.add(this.ingresarAutomovilFrame); //Agregar el frame creado al desktopPane (Se debe utilizar el objeto desktopPane porque es all� donde st� agregado el frame).
+		this.ingresarAutomovilFrame = new IngresarAutomovilFrame(); //Proceso de creación del nuevo frame (Ya que el anterior se borró).
+		desktopPane.add(this.ingresarAutomovilFrame); //Agregar el frame creado al desktopPane (Se debe utilizar el objeto desktopPane porque es allí donde está agregado el frame).
 		this.ingresarAutomovilFrame.setVisible(true); //Hacer visible el nuevo frame.
 	}
 
 	//Codigo a ejecutar del Listener del componente JMenuItem llamado (Mostrar Automovil).
 	private void showFrameMostrarAutomovil(ActionEvent e) {
-		if(this.mostrarAutomovilFrame != null) { //Verifica si el usuario ya habia abierto el frame antes (Esta es la variable que est� arriba como atributo).
-			desktopPane.remove(this.mostrarAutomovilFrame); //Esta instrucci�n elimina el frame existente si el usuario ya lo tenia abierto (Se debe utilizar el objeto desktopPane porque es all� donde st� agregado el frame).
+		if (this.mostrarAutomovilFrame != null) { //Verifica si el usuario ya habia abierto el frame antes (Esta es la variable que está arriba como atributo).
+			desktopPane.remove(this.mostrarAutomovilFrame); //Esta instrucción elimina el frame existente si el usuario ya lo tenia abierto (Se debe utilizar el objeto desktopPane porque es allí donde está agregado el frame).
 		}
-		this.mostrarAutomovilFrame = new MostrarAutomovilFrame(); //Proceso de creaci�n del nuevo frame (Ya que el anterior se borr�).
-		desktopPane.add(this.mostrarAutomovilFrame); //Agregar el frame creado al desktopPane (Se debe utilizar el objeto desktopPane porque es all� donde st� agregado el frame).
+		this.mostrarAutomovilFrame = new MostrarAutomovilFrame(); //Proceso de creación del nuevo frame (Ya que el anterior se borró).
+		desktopPane.add(this.mostrarAutomovilFrame); //Agregar el frame creado al desktopPane (Se debe utilizar el objeto desktopPane porque es allí donde está agregado el frame).
 		this.mostrarAutomovilFrame.setVisible(true); //Hacer visible el nuevo frame.
 		
 	}
 	
 	//Codigo a ejecutar del Listener del componente JMenuItem llamado (Eliminar Automovil).
 	private void showFrameEliminarAutomovil(ActionEvent e) {
-		if(this.eliminarAutomovilFrame != null) { //Verifica si el usuario ya habia abierto el frame antes (Esta es la variable que est� arriba como atributo).
-			desktopPane.remove(this.eliminarAutomovilFrame); //Esta instrucci�n elimina el frame existente si el usuario ya lo tenia abierto (Se debe utilizar el objeto desktopPane porque es all� donde st� agregado el frame).
+		if (this.eliminarAutomovilFrame != null) { //Verifica si el usuario ya habia abierto el frame antes (Esta es la variable que está arriba como atributo).
+			desktopPane.remove(this.eliminarAutomovilFrame); //Esta instrucción elimina el frame existente si el usuario ya lo tenia abierto (Se debe utilizar el objeto desktopPane porque es allí donde está agregado el frame).
 		}
-		this.eliminarAutomovilFrame = new EliminarAutomovilFrame(); //Proceso de creaci�n del nuevo frame (Ya que el anterior se borr�).
-		desktopPane.add(this.eliminarAutomovilFrame); //Agregar el frame creado al desktopPane (Se debe utilizar el objeto desktopPane porque es all� donde st� agregado el frame).
+		this.eliminarAutomovilFrame = new EliminarAutomovilFrame(); //Proceso de creación del nuevo frame (Ya que el anterior se borró).
+		desktopPane.add(this.eliminarAutomovilFrame); //Agregar el frame creado al desktopPane (Se debe utilizar el objeto desktopPane porque es allí donde está agregado el frame).
 		this.eliminarAutomovilFrame.setVisible(true); //Hacer visible el nuevo frame.
 		
 	}
 	
 	//Codigo a ejecutar del Listener del componente JMenuItem llamado (Modificar Automoviles).
 	private void showFrameModificarAutomoviles(ActionEvent e) {
-		if(this.modificarAutomovilFrame != null) { //Verifica si el usuario ya habia abierto el frame antes (Esta es la variable que est� arriba como atributo).
-			desktopPane.remove(this.modificarAutomovilFrame); //Esta instrucci�n elimina el frame existente si el usuario ya lo tenia abierto (Se debe utilizar el objeto desktopPane porque es all� donde st� agregado el frame).
+		if (this.modificarAutomovilFrame != null) { //Verifica si el usuario ya habia abierto el frame antes (Esta es la variable que está arriba como atributo).
+			desktopPane.remove(this.modificarAutomovilFrame); //Esta instrucción elimina el frame existente si el usuario ya lo tenia abierto (Se debe utilizar el objeto desktopPane porque es allí donde está agregado el frame).
 		}
-		this.modificarAutomovilFrame = new ModificarAutomovilFrame(); //Proceso de creaci�n del nuevo frame (Ya que el anterior se borr�).
-		desktopPane.add(this.modificarAutomovilFrame); //Agregar el frame creado al desktopPane (Se debe utilizar el objeto desktopPane porque es all� donde st� agregado el frame).
+		this.modificarAutomovilFrame = new ModificarAutomovilFrame(); //Proceso de creación del nuevo frame (Ya que el anterior se borró).
+		desktopPane.add(this.modificarAutomovilFrame); //Agregar el frame creado al desktopPane (Se debe utilizar el objeto desktopPane porque es allí donde está agregado el frame).
 		this.modificarAutomovilFrame.setVisible(true); //Hacer visible el nuevo frame.
 	}
 
